aca8794b60985135b54d0ce1af46ad8b8c539b69
Nikita Shulha <shulgamalthael@gmail.com>
2023-01-22 15:47:44 +0200

    init code.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+/* react */
+import { useCallback, useMemo } from 'react';
+
+/* css */
 import './App.css';
 
 function App() {
+  const getUser = useCallback((user) => {
+    const result = user || { name: "Nikita", age: "22" };
+    if(!user) {window.user = result;}
+    return result;
+  }, []);
+
+  const user = useMemo(() => {
+    const _user = getUser();
+    _user.name = "OWL";
+    return _user;
+  }, []);
+  
+  console.log(user === window.user);
+
+  user.age = "25";
+
+  console.log(user === window.user);
+
+  console.log("Rerendered");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.name} / {user.age}
     </div>
   );
 }
